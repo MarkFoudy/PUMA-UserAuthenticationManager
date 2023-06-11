c3fb8e8b2839ba654807054ac63e24efb9e879b5
BostonThomist <29990433+MarkFoudy@users.noreply.github.com>
2023-06-11 19:42:21 -0400

    I removed the email address

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/17746/Desktop/Final_Programs_for_website/CS5008_CapstoneFinal
+# Install script for directory: C:/Users/17746/Desktop/Final_Programs_for_website/PUMA
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/17746/Desktop/Final_Programs_for_website/CS5008_CapstoneFinal/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/17746/Desktop/Final_Programs_for_website/PUMA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
